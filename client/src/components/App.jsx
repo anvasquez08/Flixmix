b32ea40048112feb0fd2f7afe8a55a6b1aaa3632
guillermo adrian <gjadriant@gmail.com>
2018-07-18 10:47:24 -0400

    normal stuff

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,29 +1,32 @@
-import React from 'react';
-import axios from 'axios';
-
-import Signup from './Signup.jsx';
-import Profile from './Profile.jsx';
-import Search from '../components/Search.jsx'
-import SearchResults from '../components/SearchResults.jsx'
-import Playlist from './Playlist.jsx';
-import PlayListViewer from './PlaylistViewer.jsx';
-import Navbar from './Navbar.jsx';
+import React from "react";
+import axios from "axios";
+import Login from "./Login.jsx";
 
 
-class App extends React.Component{
+import Signup from "./Signup.jsx";
+import Profile from "./Profile.jsx";
+import Search from "../components/Search.jsx";
+import SearchResults from "../components/SearchResults.jsx";
+import Playlist from "./Playlist.jsx";
+import PlayListViewer from "./PlaylistViewer.jsx";
+import Navbar from "./Navbar.jsx";
+
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      user_id: '',
-      username: '',
+      user_id: "",
+      username: "",
       isLoggedIn: false,
       searchResults: [],
-      userInput: '',
+      userInput: "",
       playlist: [],
-      user: 'placeholder',
-      toggleView: true
-    }
+      user: "placeholder",
+      toggleView: true,
+      loginHover: false,
+    };
     this.login = this.login.bind(this);
+    this.signup = this.signup.bind(this);
     this.logout = this.logout.bind(this);
     this.updateUserInput = this.updateUserInput.bind(this);
     this.searchOnSubmit = this.searchOnSubmit.bind(this);
@@ -32,48 +35,48 @@ class App extends React.Component{
     this.movePlaylistItemDown = this.movePlaylistItemDown.bind(this);
     this.movePlaylistItemUp = this.movePlaylistItemUp.bind(this);
     this.sendPlaylist = this.sendPlaylist.bind(this);
-
+    this.handleHover = this.handleHover.bind(this);
   }
-
 
   // Login, Logout, Signup Functions
 
   login(e, username, password) {
-    e.preventDefault()
-    let body = {username,  password}
-    axios.post('flixmix/login', body)
-    .then(response => { 
-      console.log(response.data[0].users_id)
-      this.setState(
-      {
-      user_id: response.data[0].users_id,
-      username: response.data[0].username,
-      isLoggedIn: true
-      }
-    )})
-    .catch(err => console.log(err))
+    e.preventDefault();
+    let body = { username, password };
+    axios
+      .post("flixmix/login", body)
+      .then(response => {
+        console.log(response.data[0].users_id);
+        this.setState({
+          user_id: response.data[0].users_id,
+          username: response.data[0].username,
+          isLoggedIn: true
+        });
+      })
+      .catch(err => console.log(err));
   }
 
   logout() {
     this.setState({
-      user_id: '',
+      user_id: "",
       isLoggedIn: false
-    })
+    });
   }
 
   signup(e, username, password) {
-    e.preventDefault()
-    let body = {username,  password}
-    axios.post('flixmix/signup', body)
-    .then(response => { this.setState(
-      {
-      user_id: response.data[0].user_id,
-      username: response.data[0].username,
-      isLoggedIn: true
-      }
-    )})
-    .then((result) => console.log("Console logging signup: ", result))
-    .catch(err => console.log(err))
+    e.preventDefault();
+    let body = { username, password };
+    axios
+      .post("flixmix/signup", body)
+      .then(response => {
+        this.setState({
+          user_id: response.data[0].user_id,
+          username: response.data[0].username,
+          isLoggedIn: true
+        });
+      })
+      .then(result => console.log("Console logging signup: ", result))
+      .catch(err => console.log(err));
   }
 
   // Search component helper functions
@@ -85,13 +88,12 @@ class App extends React.Component{
   }
 
   searchOnSubmit() {
-    let body = {search: this.state.userInput}
-    axios.post('flixmix/search', body)
-    .then((result) => {
+    let body = { search: this.state.userInput };
+    axios.post("flixmix/search", body).then(result => {
       this.setState({
         searchResults: result.data.results
-      })
-    })
+      });
+    });
   }
 
   // Playlist helper functions: add to/delete from playlist, reorder items up/down
@@ -102,95 +104,116 @@ class App extends React.Component{
 
     this.setState({
       playlist: newPlaylist
-    })
+    });
   }
 
   deleteFromPlaylist(index) {
     let playlistCopy = this.state.playlist.slice();
-    let newPlaylist = [...playlistCopy.slice(0, index), ...playlistCopy.slice(index+1)]
+    let newPlaylist = [
+      ...playlistCopy.slice(0, index),
+      ...playlistCopy.slice(index + 1)
+    ];
 
     this.setState({
       playlist: newPlaylist
-    })
-
+    });
   }
 
   movePlaylistItemUp(array, index) {
     if (index === 0) {
-      return array
-    } else {
-      let modified = array.slice();
-      let temp = modified[index];
-      modified[index] = modified[index-1];
-      modified[index-1] = temp;
-
-      this.setState({
-        playlist: modified
-      })
-
-    }
-  }
-
-  movePlaylistItemDown(array, index) {
-    if (index === array.length-1) {
       return array;
     } else {
       let modified = array.slice();
       let temp = modified[index];
-      modified[index] = modified[index+1];
-      modified[index+1] = temp;
+      modified[index] = modified[index - 1];
+      modified[index - 1] = temp;
 
       this.setState({
         playlist: modified
-      })
-
+      });
     }
   }
 
-  sendPlaylist(){
-    console.log('movies to send', this.state.playlist)
-    axios.post('/flixmix/createPlaylist', {movieArr: this.state.playlist, user_id: this.state.user_id})
+  movePlaylistItemDown(array, index) {
+    if (index === array.length - 1) {
+      return array;
+    } else {
+      let modified = array.slice();
+      let temp = modified[index];
+      modified[index] = modified[index + 1];
+      modified[index + 1] = temp;
+
+      this.setState({
+        playlist: modified
+      });
+    }
   }
 
+  sendPlaylist() {
+    console.log("movies to send", this.state.playlist);
+    axios.post("/flixmix/createPlaylist", {
+      movieArr: this.state.playlist,
+      user_id: this.state.user_id
+    });
+  }
 
-  
+  handleHover(){
+    this.setState({loginHover: !this.state.loginHover})
+  }
+
   render() {
     return (
       <div>
-      <Navbar login={this.login} signup={this.signup} />
-      <div className="NavBar"><center>      
-      <button onClick={() => this.setState({toggleView: !this.state.toggleView})}>
-        {this.state.toggleView ? "Playlist Viewer" : "Create Playlist"}
-      </button>
-      <button onClick={this.logout}>Logout</button></center>
-      </div>
+        <Navbar handleHover={this.handleHover}  />
+        {this.state.loginHover ? <Login login={this.login} signup={this.signup} /> : null}
+        <div className="NavBar">
+          <center>
+            <button
+              onClick={() =>
+                this.setState({ toggleView: !this.state.toggleView })
+              }
+            >
+              {this.state.toggleView ? "Playlist Viewer" : "Create Playlist"}
+            </button>
+            <button onClick={this.logout}>Logout</button>
+          </center>
+        </div>
 
-      <p />
-      
-      {this.state.toggleView ? 
-      (<div>
-        <center>
-        <Search userInput={this.state.userInput} updateUserInput={this.updateUserInput} searchOnSubmit={this.searchOnSubmit}/>
-        <SearchResults movies={this.state.searchResults} add={this.addToPlaylist}/>
-        </center>
-        <Playlist movies={this.state.playlist} delete={this.deleteFromPlaylist} moveUp={this.movePlaylistItemUp} moveDown={this.movePlaylistItemDown}/>
-        <button onClick={this.sendPlaylist}>Create Playlist</button>
+        <p />
+        {this.state.toggleView ? (
+          <div>
+            <center>
+              <Search
+                userInput={this.state.userInput}
+                updateUserInput={this.updateUserInput}
+                searchOnSubmit={this.searchOnSubmit}
+              />
+              <SearchResults
+                movies={this.state.searchResults}
+                add={this.addToPlaylist}
+              />
+            </center>
+            <Playlist
+              movies={this.state.playlist}
+              delete={this.deleteFromPlaylist}
+              moveUp={this.movePlaylistItemUp}
+              moveDown={this.movePlaylistItemDown}
+            />
+            <button onClick={this.sendPlaylist}>Create Playlist</button>
+          </div>
+        ) : (
+          <div>
+            <PlayListViewer />
+          </div>
+        )}
       </div>
-      )
-      : (<div>
-        <PlayListViewer />
-      </div>)
-      }
-      </div>
-      )
+    );
   }
 }
 
-
 export default App;
 
-
 // Note for styling: there are <center> tags throughout
-  // Feel free to remove as you add classNames to components
+// Feel free to remove as you add classNames to components
 
 // Profile component temporarily removed - can add back with <Profile />
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 class Login extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      username: '',
-      password: ''
-    }
+      username: "",
+      password: ""
+    };
   }
 
   setLoginState(e) {
@@ -15,33 +15,86 @@ class Login extends React.Component {
     let val = e.target.value;
     this.setState({
       [name]: val
-    })
+    });
   }
 
   render() {
     return (
-      <div>
-          <form>
-            <input type="text" name="username" value={this.state.username} onChange={(e) => {this.setLoginState(e)}}/>
-            <input type="password" name="password" value={this.state.password} onChange={(e) => {this.setLoginState(e)}}/>
-            <button onClick={(e) => {
-              this.props.login(e, this.state.username, this.state.password)
-              this.setState({
-                username: '',
-                password: ''
-              })
-              }}>Login</button>
-            <button onClick={(e) => {
-              this.props.signup(e, this.state.username, this.state.password)
-              this.setState({
-                username: '',
-                password: ''
-              })              
-            }}>Signup</button>
-          </form>
+      <div className="column is-ancestor is-4 is-offset-8">
+        <div className="column is-parent is-vertical field ">
+          <p className="control has-icons-left column is-parent is-vertical arrow_box">
+            <input
+              className="input column is-child"
+              type="email"
+              placeholder="&#xf0e0; Username"
+              style={{ fontFamily: "Arial, FontAwesome",
+              marginBottom: "5px"
+            }}
+            />
+            <input
+              className="input column is-child"
+              type="password"
+              placeholder="&#xf0c1; Password"
+              style={{
+                fontFamily: "Arial, FontAwesome",
+                marginBottom: "5px"
+              }}
+            />
+            <button
+              className="button is-success"
+              onClick={e => {
+                this.props.login(e, this.state.username, this.state.password);
+                this.setState({
+                  username: "",
+                  password: ""
+                });
+              }}
+              style={{
+                marginRight: "5px"
+              }}
+            >
+              {" "}
+              Log in{" "}
+            </button>
+            <button
+              className="button is-info"
+              onClick={e => {
+                this.props.signup(e, this.state.username, this.state.password);
+                this.setState({
+                  username: "",
+                  password: ""
+                });
+              }}
+            >
+              {" "}
+              Sign Up{" "}
+            </button>
+          </p>
+        </div>
       </div>
-      )
+    );
   }
 }
 
 export default Login;
+
+// <div>
+// <form>
+//   <input type="text" name="username" value={this.state.username} onChange={(e) => {this.setLoginState(e)}}/>
+//   <input type="password" name="password" value={this.state.password} onChange={(e) => {this.setLoginState(e)}}/>
+//   <button className="button is-success" onClick={(e) => {
+//     this.props.login(e, this.state.username, this.state.password)
+//     this.setState({
+//       username: '',
+//       password: ''
+//     })
+//     }}>Login</button>
+//   <button className="button is-success" onClick={(e) => {
+//     this.props.signup(e, this.state.username, this.state.password)
+//     this.setState({
+//       username: '',
+//       password: ''
+//     })
+//   }}>Signup</button>
+// </form>
+// </div>
