9d72ddb780299b9800adacbdbbce4d15d212813e
Ana Vasquez <anvasquez08@gmail.com>
2018-07-18 19:00:18 -0400

    Misc. edits

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
-import Explore from './components/Explore.jsx';
 
-ReactDOM.render(<Explore />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
